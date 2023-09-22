2d487d49795bd68442b7a5bcc2ba1a33e58cb263
lyw142 <william.lay.2021@scis.smu.edu.sg>
2023-09-23 02:29:36 +0800

    Update all

---
+++ backend/create_tables.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from models import Role, Staff, Skill, Staff_Skill, RoleSkillMapping, Application, RoleListing
+from models import db
+
+app = Flask('api_app')
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/SPM'
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_size': 100,
+                                               'pool_recycle': 280}
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# db = SQLAlchemy(app)
+
+
+# Define your classes as mentioned in your code here
+
+if __name__ == '__main__':
+    # This will create the database tables based on your class definitions
+    db.init_app(app)
+    with app.app_context():
+        db.create_all() 
---
+++ backend/models.py
@@ -0,0 +1,197 @@
+"""
+models.py
+- Data classes for the application
+"""
+
+from email.policy import default
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+    
+## STAFF ##
+class Staff(db.Model):
+    __tablename__ = 'staff'
+    __table_args__ = {'mysql_engine': 'InnoDB'}  # Specify InnoDB engine for this table
+
+    Staff_ID = db.Column(db.Integer, primary_key=True)
+    Staff_FName = db.Column(db.String(50), nullable=False)
+    Staff_LName = db.Column(db.String(50), nullable=False)
+    Email = db.Column(db.String(50), nullable=False)
+    Password = db.Column(db.String(50), nullable=False)
+    Role_ID = db.Column(db.Integer, db.ForeignKey('role.Role_ID'), nullable=False)
+    Access_Rights = db.Column(db.Integer, nullable=False)
+    
+    #regi = db.relationship('Registration', backref='regi', cascade='all,delete-orphan')
+    
+    def __init__(self, Staff_ID, Staff_FName, Staff_LName, Email, Password, Access_Rights, Role_ID):
+        self.Staff_ID = Staff_ID
+        self.Staff_FName = Staff_FName
+        self.Staff_LName = Staff_LName
+        self.Email = Email
+        self.Password = Password
+        self.Access_Rights = Access_Rights
+        self.Role_ID = Role_ID
+
+    def to_json(self):
+        return {
+            "Staff_ID": self.Staff_ID,
+            "Staff_FName": self.Staff_FName,
+            "Staff_LName": self.Staff_LName,
+            "Email": self.Email,
+            "Password": self.Password,
+            "Access_Rights": self.Access_Rights,
+            "Role_ID": self.Role_ID
+        }
+
+## STAFF_SKILL ##
+class Staff_Skill(db.Model):
+    __tablename__ = 'staffskill'
+    __table_args__ = {'mysql_engine': 'InnoDB'}  # Specify InnoDB engine for this table
+
+    Staff_ID = db.Column(db.Integer, db.ForeignKey('staff.Staff_ID', ondelete='CASCADE'), primary_key=True)
+    Skill_ID = db.Column(db.Integer, db.ForeignKey('skill.Skill_ID', ondelete='CASCADE'), primary_key=True)
+
+    def __init__(self, Staff_ID, Skill_Name):
+        self.Staff_ID = Staff_ID
+        self.Skill_Name = Skill_Name
+
+    # skill = db.relationship('Skill', backref='staff_skill')
+
+    # specify how to represent our Staff_Skill object as a JSON string
+    def json(self):
+        return {
+                "Staff_ID": self.Staff_ID, 
+                "Skill_Name": self.Skill_Name,
+            }
+       
+## SKILL ##
+class Skill(db.Model):
+    __tablename__ = 'skill'
+    __table_args__ = {'mysql_engine': 'InnoDB'}  # Specify InnoDB engine for this table
+
+    Skill_ID = db.Column(db.Integer, primary_key=True)
+    Skill_Name = db.Column(db.String(20), nullable=False)
+    Skill_Status = db.Column(db.String(15), nullable=False, default = 'Active')
+
+    def __init__(self, Skill_ID, Skill_Name,Skill_Status):
+        self.Skill_ID = Skill_ID
+        self.Skill_Name = Skill_Name
+        self.Skill_Status = Skill_Status
+
+    # specify how to represent our book object as a JSON string
+    def json(self):
+        dto = {
+                "Skill_ID": self.Skill_ID,
+                "Skill_Name": self.Skill_Name,
+                "Skill_Status": self.Skill_Status
+            }
+            
+        return dto
+
+## ROLE ##
+class Role(db.Model):
+    __tablename__ = 'role'
+    __table_args__ = {'mysql_engine': 'InnoDB'}  # Specify InnoDB engine for this table
+
+    Role_ID = db.Column(db.Integer, primary_key=True)
+    Role_Name = db.Column(db.String(20), nullable=False)
+    Role_Description = db.Column(db.String(100), nullable=False)
+    Salary = db.Column(db.String(20), nullable=False)
+    Dept = db.Column(db.String(50), nullable=False)
+
+    def __init__(self, Role_ID, Role_Name, Role_Description, Salary, Dept):
+        self.Role_ID = Role_ID
+        self.Role_Name = Role_Name
+        self.Role_Description = Role_Description
+        self.Salary = Salary
+        self.Dept = Dept
+
+    def to_json(self):
+        return {
+            "Role_ID": self.Role_ID,
+            "Role_Name": self.Role_Name,
+            "Role_Description": self.Role_Description,
+            "Salary": self.Salary,
+            "Dept": self.Dept
+        }
+
+## Role_skill_mapping #
+class RoleSkillMapping(db.Model):
+    __tablename__ = 'role_skill_mapping'
+    __table_args__ = {'mysql_engine': 'InnoDB'}  # Specify InnoDB engine for this table
+
+    Skill_ID = db.Column(db.Integer, db.ForeignKey('skill.Skill_ID', ondelete='CASCADE'), primary_key=True)
+    Role_ID = db.Column(db.Integer, db.ForeignKey('role.Role_ID', ondelete='CASCADE'), primary_key=True)
+
+    def __init__(self, Skill_ID, Role_ID):
+        self.Role_ID = Role_ID
+        self.Skill_ID = Skill_ID
+
+    # specify how to represent our book object as a JSON string
+    def json(self):
+        return {
+                "Role_ID": self.Role_ID,
+                "Skill_ID": self.Skill_ID
+            }
+    
+## Application ##
+class Application(db.Model):
+    __tablename__ = 'application'
+    __table_args__ = {'mysql_engine': 'InnoDB'}  # Specify InnoDB engine for this table
+
+    Application_ID = db.Column(db.Integer, primary_key=True)
+    Application_Date = db.Column(db.Date, nullable=False)
+    Application_Status = db.Column(db.String(20), nullable=False)
+    Staff_ID = db.Column(db.Integer, db.ForeignKey('staff.Staff_ID', ondelete='CASCADE'))
+    Listing_ID = db.Column(db.Integer, db.ForeignKey('role_listing.Listing_ID', ondelete='CASCADE'))
+
+    #staff = db.relationship('Staff', backref='applications')
+    #role = db.relationship('Role', backref='applications')
+    
+    def __init__(self, Application_ID, Application_Date, Application_Status, Staff_ID, Listing_ID):
+        self.Application_ID = Application_ID
+        self.Application_Date = Application_Date
+        self.Application_Status = Application_Status
+        self.Staff_ID = Staff_ID
+        self.Listing_ID = Listing_ID
+
+    # specify how to represent our book object as a JSON string
+    def json(self):
+        return {
+                    "Application_ID": self.Application_ID, 
+                    "Application_Date": self.Application_Date,
+                    "Application_Status": self.Application_Status,
+                    "Staff_ID": self.Staff_ID,
+                    "Listing_ID": self.Listing_ID
+                }
+
+## Role_Listing ##
+class RoleListing(db.Model):
+    __tablename__ = 'role_listing'
+    __table_args__ = {'mysql_engine': 'InnoDB'}  # Specify InnoDB engine for this table
+
+    Listing_ID = db.Column(db.Integer, primary_key=True)
+    Deadline = db.Column(db.Date, nullable=False)
+    Date_Posted = db.Column(db.Date, nullable=False)
+    Hiring_Manager = db.Column(db.Integer, db.ForeignKey('staff.Staff_ID', ondelete='CASCADE'), nullable=False)
+    Role_ID = db.Column(db.Integer, db.ForeignKey('role.Role_ID', ondelete='CASCADE'), nullable=False)
+
+    #staff = db.relationship('Staff', backref='applications')
+    #role = db.relationship("Role", back_populates="role_listing")
+    
+    def __init__(self, Listing_ID, Deadline, Date_Posted, Hiring_Manager, Role_ID):
+        self.Listing_ID = Listing_ID
+        self.Deadline = Deadline
+        self.Date_Posted = Date_Posted
+        self.Hiring_Manager = Hiring_Manager
+        self.Role_ID = Role_ID
+
+    def to_json(self):
+        return {
+            "Listing_ID": self.Listing_ID,
+            "Deadline": self.Deadline, 
+            "Date_Posted": self.Date_Posted,
+            "Hiring_Manager": self.Hiring_Manager,
+            "Role_ID": self.Role_ID
+        }
