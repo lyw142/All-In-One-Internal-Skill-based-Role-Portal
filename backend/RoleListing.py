0ee03f6702f84ff0c838f1ca189f2a2150f41bfe
lyw142 <william.lay.2021@scis.smu.edu.sg>
2023-09-26 21:26:31 +0800

    Update RoleListing.py to include showing Role Name and Hiring Manager

--- backend/RoleListing.py
+++ backend/RoleListing.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from sqlalchemy import desc
+from models import Role, Staff;
 
 app = Flask(__name__)
 
@@ -58,6 +59,33 @@ def findAllJobListings():
         return jsonify({
             "message": "No job listings found."
         }), 404
+    
+@app.route("/joblistings2")
+def findAllJobListings2():
+    # Perform joins to retrieve role listings with Hiring Manager and Role Name
+    query = (
+        db.session.query(RoleListing, Staff.Staff_FName, Staff.Staff_LName, Role.Role_Name)
+        .join(Staff, RoleListing.Hiring_Manager == Staff.Staff_ID)
+        .join(Role, RoleListing.Role_ID == Role.Role_ID)
+        .order_by(desc(RoleListing.Date_Posted))
+    )
+
+    # Execute the query and retrieve the results
+    results = query.all()
+
+    # Convert the results into a JSON format
+    role_listings_json = []
+    for role_listing, hiring_manager_fname, hiring_manager_lname, role_name in results:
+        role_listing_data = {
+            "Listing_ID": role_listing.Listing_ID,
+            "Deadline": str(role_listing.Deadline),
+            "Date_Posted": str(role_listing.Date_Posted),
+            "Hiring_Manager": hiring_manager_fname + " " + hiring_manager_lname,
+            "Role_Name": role_name
+        }
+        role_listings_json.append(role_listing_data)
+
+    return role_listings_json;
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
