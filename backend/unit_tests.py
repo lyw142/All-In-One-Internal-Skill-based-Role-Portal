2627b5afb42b297f91b4cb274aefaeb9f09696de
lyw142 <william.lay.2021@scis.smu.edu.sg>
2023-11-02 12:50:21 +0800

    Update unit_tests.py

--- backend/unit_tests.py
+++ backend/unit_tests.py
@@ -14,22 +14,22 @@ class TestAccessControl(unittest.TestCase):
 
 class TestStaff(unittest.TestCase):
     def test(self):
-        s1 = Staff(Staff_FName="John", Staff_LName='Lee', Email="join@gmail.com", Password="1234", Role_ID=1, Country="Singapore", Dept='IT', Access_Rights=1)
+        s1 = Staff(Staff_ID=1, Staff_FName="John", Staff_LName='Lee', Email="join@gmail.com", Password="1234", Role_ID=1, Country="Singapore", Dept='IT', Access_Rights=1)
         self.assertEqual(s1.get_staff_details(), {"Staff_ID":s1.Staff_ID, "Staff_FName":s1.Staff_FName, "Staff_LName":s1.Staff_LName, "Email":s1.Email, "Password":s1.Password, "Role_ID":s1.Role_ID, "Country":s1.Country, "Dept":s1.Dept, "Access_Rights":s1.Access_Rights})
 
 class TestManager(unittest.TestCase):
     def test(self):
-        m1 = Manager(Staff_FName="John", Staff_LName='Lee', Email="join@gmail.com", Password="1234", Role_ID=1, Country="Singapore", Dept='IT', Access_Rights=1,ResponsibleTeam="Dev", Reporting_Manager=1)
+        m1 = Manager(Staff_ID=3,Staff_FName="John", Staff_LName='Lee', Email="join@gmail.com", Password="1234", Role_ID=1, Country="Singapore", Dept='IT', Access_Rights=1,ResponsibleTeam="Dev", Reporting_Manager=1)
         self.assertEqual(m1.get_manager_details(), {"Staff_ID":m1.Staff_ID, "Responsible_Team":m1.ResponsibleTeam, "Reporting_Manager":m1.Reporting_Manager})
 
 class TestDirector(unittest.TestCase):
     def test(self):
-        d1 = Director(Staff_FName="John", Staff_LName='Lee', Email="join@gmail.com", Password="1234", Role_ID=1, Country="Singapore", Dept='IT', Access_Rights=1, ResponsibelDept="IT", Reporting_Manager=1)
+        d1 = Director(Staff_ID=2,Staff_FName="John", Staff_LName='Lee', Email="join@gmail.com", Country="Singapore", Dept='IT', Password="1234", Role_ID=1,  Access_Rights=1, ResponsibelDept="IT", Reporting_Manager=1)
         self.assertEqual(d1.get_director_details(), {"Staff_ID":d1.Staff_ID, "Responsible_Department":d1.ResponsibleDept, "Reporting_Manager":d1.Reporting_Manager})
 
 class TestNormalStaff(unittest.TestCase):
     def test(self):
-        ns1 = NormalStaff(Staff_FName="John", Staff_LName='Lee', Email="John@gmail.com", Password="1234", Role_ID=1, Country="Singapore", Dept='IT', Access_Rights=1, Reporting_Manager=1)
+        ns1 = NormalStaff(Staff_ID=5,Staff_FName="John", Staff_LName='Lee', Email="John@gmail.com", Country="Singapore", Dept='IT', Password="1234", Role_ID=1, Access_Rights=1, Reporting_Manager=1)
         self.assertEqual(ns1.get_normal_staff_details(), {"Staff_ID":ns1.Staff_ID, "Reporting_Manager":ns1.Reporting_Manager})
 
 class TestStaffSkill(unittest.TestCase):
@@ -44,7 +44,7 @@ class TestSkill(unittest.TestCase):
 
 class TestRole(unittest.TestCase):
     def test(self): 
-        r1 = Role(Role_Name="Developer", Role_Responsibilities="Dev")
+        r1 = Role(Role_ID=1,Role_Name="Developer", Role_Responsibilities="Dev")
         self.assertEqual(
             r1.get_role_details(), 
             {"Role_Name": "Developer", "Role_Responsibilities": "Dev"}
