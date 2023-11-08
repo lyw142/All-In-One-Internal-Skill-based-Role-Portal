a264f951ee00d951eda9156e4c0cf1db636ac28b
kaeeezia <91945302+kaeeezia@users.noreply.github.com>
2023-11-08 18:40:19 +0800

    integrated testing candidates

--- frontend/src/components/Candidates.test.js
+++ frontend/src/components/Candidates.test.js
@@ -1,247 +1,240 @@
+import { createApp } from "vue";
 import { mount } from "@vue/test-utils";
-import Candidates from "./Candidates.vue"; 
-
-import { afterAll, afterEach, beforeAll } from "vitest";
+import Candidates from "./Candidates.vue";
+import Cookies from "js-cookie";
+import axios from "axios";
 import { setupServer } from "msw/node";
-import { http } from "msw";
+import { rest } from "msw";
+import { test } from "vitest";
 
-export const restHandlers = [
-  http.get("http://127.0.0.1:5000/api/getapplications", (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json({ data: [] }));
+import { JSDOM } from "jsdom";
+
+const { window } = new JSDOM("<body></body>");
+global.document = window.document;
+global.window = window;
+
+const server = setupServer(
+  rest.get("http://127.0.0.1:5000/api/getapplications", (req, res, ctx) => {
+    return res(ctx.json([])); // returns an empty array
   }),
-  http.get("http://127.0.0.1:5000/api/getRoleListing/1", (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json({ data: [] }));
-  }),
-  http.get(
-    "http://127.0.0.1:5000/api/getApplicantsBySkillMatch/1",
+  rest.get(
+    "http://127.0.0.1:5000/api/getRoleListing/:listingId",
     (req, res, ctx) => {
-      return res(ctx.status(200), ctx.json({ data: [] }));
+      return res(ctx.json([])); // returns an empty array
     }
   ),
-];
+  rest.get(
+    "http://127.0.0.1:5000/api/getApplicantsBySkillMatch/:listingId",
+    (req, res, ctx) => {
+      return res(ctx.json([])); // returns an empty array
+    }
+  ),
+  rest.get("http://127.0.0.1:5000/api/skills", (req, res, ctx) => {
+    return res(ctx.json([])); // returns an empty array
+  })
+);
 
-const server = setupServer(...restHandlers);
-
-// Start server before all tests
-beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
-
-//  Close server after all tests
-afterAll(() => server.close());
-
-// Reset handlers after each test `important for test isolation`
-afterEach(() => server.resetHandlers());
-
-const mockRoute = {
-  // params: {
-  //   id: 1
-  // }
-};
-const mockRouter = {
-  push: vi.fn(),
+const setup = () => {
+  server.listen();
 };
 
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-describe("Candidates", () => {
-  it("renders successfully", () => {
-    const wrapper = mount(Candidates, {
-      global: {
-        mocks: {
-          $route: mockRoute,
-          $router: mockRouter,
-        },
-      },
-      stubs: ["router-link", "router-view"],
-    });
-    expect(wrapper.exists()).toBe(true);
+const reset = () => {
+  server.resetHandlers();
+};
+
+const tearDown = () => {
+  server.close();
+};
+
+const runTest = async (callback) => {
+  setup();
+  await callback();
+  reset();
+  tearDown();
+};
+
+test("Candidates.vue", () => {
+  // All your test cases go here
+  test("fetchApplications", async () => {
+    server.use(
+      rest.get("http://127.0.0.1:5000/api/getapplications", (req, res, ctx) => {
+        return res(
+          ctx.json([
+            {
+              Staff_ID: 1,
+              Listing_ID: 1,
+              Role_Name: "Software Engineer",
+            },
+          ])
+        );
+      })
+    );
+
+    const wrapper = mount(Candidates);
+
+    await wrapper.vm.fetchApplications();
+
+    expect(wrapper.vm.$data.applications).toHaveLength(1);
+
+    expect(wrapper.vm.$data.roles).toHaveLength(1);
   });
 
-  it("initializes data correctly", () => {
-    const wrapper = mount(Candidates, {
-      global: {
-        mocks: {
-          $route: mockRoute,
-          $router: mockRouter,
-        },
-      },
-      stubs: ["router-link", "router-view"],
-    });
-    const data = wrapper.vm.$data;
+  test("fetchRoleListing", async () => {
+    server.use(
+      rest.get(
+        "http://127.0.0.1:5000/api/getRoleListing/:listingId",
+        (req, res, ctx) => {
+          return res(
+            ctx.json([
+              {
+                Listing_ID: 1,
+                Role_Name: "Software Engineer",
+              },
+            ])
+          );
+        }
+      )
+    );
 
-    // Assert that your data properties are initialized as expected
-    expect(data.applications).toEqual([]);
-    expect(data.roles).toEqual([]);
-    // Add more assertions for other data properties
+    const wrapper = mount(Candidates);
+
+    await wrapper.vm.fetchRoleListing(1);
+
+    expect(wrapper.vm.$data.applications[0].roleListing).toBeTruthy();
   });
 
-  it("calculates application count correctly", async () => {
-    // Mount your component
-    const { vm } = mount(Candidates, {
-      global: {
-        mocks: {
-          $route: mockRoute,
-          $router: mockRouter,
+  test("countApplications", () => {
+    const wrapper = mount(Candidates);
+
+    wrapper.setData({
+      applications: [
+        {
+          Listing_ID: 1,
+          Role_Name: "Software Engineer",
         },
+      ],
+      selectedRole: {
+        Listing_ID: 1,
+        Role_Name: "Software Engineer",
       },
-      stubs: ["router-link", "router-view"],
     });
 
-    // Wait for the component to process the API response
-    await sleep(1000);
+    wrapper.vm.countApplications();
 
-    // Assert that the applications are correctly set in the component
-    expect(vm.applications.length).toEqual(0);
-
-    expect(vm.roles).toEqual([]);
-
-    // Assert that the application count is calculated correctly
-    expect(vm.applicationCount).toBe(0);
+    expect(wrapper.vm.$data.applicationCount).toBe(1);
   });
-  it("correctly filters candidates based on selected skills", async () => {
-    const wrapper = mount(Candidates, {
-      global: {
-        mocks: {
-          $route: mockRoute,
-          $router: mockRouter,
-        },
-      },
-      stubs: ["router-link", "router-view"],
-    });
-    await wrapper.vm.$nextTick();
 
-    // Simulate selecting specific skills in the component
-    wrapper.setData({ selectedSkills: ["Skill1", "Skill2"] });
+  test("getApplicantBySkillCount", async () => {
+    server.use(
+      rest.get(
+        "http://127.0.0.1:5000/api/getApplicantsBySkillMatch/:listingId",
+        (req, res, ctx) => {
+          return res(
+            ctx.json([
+              {
+                Staff_ID: 1,
+                Listing_ID: 1,
+              },
+            ])
+          );
+        }
+      )
+    );
 
-    // Assert that filteredCandidates contains only candidates with the selected skills
-    expect(wrapper.vm.filteredCandidates).toEqual([]);
+    const wrapper = mount(Candidates);
+
+    await wrapper.vm.getApplicantBySkillCount();
+
+    expect(wrapper.vm.$data.applicationsforCount).toHaveLength(1);
   });
-  it("correctly calculates application count when role changes", async () => {
-    const wrapper = mount(Candidates, {
-      global: {
-        mocks: {
-          $route: mockRoute,
-          $router: mockRouter,
-        },
-      },
-      stubs: ["router-link", "router-view"],
-    });
-    await wrapper.vm.$nextTick();
 
-    // Simulate changing the selected role in the component
-    wrapper.setData({ selectedRole: "Role 1" });
+  test("showTable", () => {
+    const wrapper = mount(Candidates);
 
-    // Assert that applicationCount is updated as expected
-    expect(wrapper.vm.applicationCount).toEqual(0);
+    wrapper.vm.showTable();
+
+    expect(wrapper.vm.$data.sortOrder).toBe(false);
+
+    expect(wrapper.vm.$data.sortCount).toBe(true);
   });
-  it("correctly sorts candidates based on selected sorting criteria", async () => {
-    const wrapper = mount(Candidates, {
-      global: {
-        mocks: {
-          $route: mockRoute,
-          $router: mockRouter,
-        },
-      },
-      stubs: ["router-link", "router-view"],
-    });
-    await wrapper.vm.$nextTick();
 
-    // Simulate selecting a sorting criteria (e.g., selectedOrderBy or selectedSkills)
-    // to trigger sorting
+  test("fetchSkills", async () => {
+    server.use(
+      rest.get("http://127.0.0.1:5000/api/skills", (req, res, ctx) => {
+        return res(
+          ctx.json([
+            {
+              Skill_ID: 1,
+              Skill_Name: "Vue.js",
+            },
+          ])
+        );
+      })
+    );
 
-    // Assert that the candidates are sorted as expected
-    expect(wrapper.vm.sortedCandidates).toEqual([]);
+    const wrapper = mount(Candidates);
+
+    await wrapper.vm.fetchSkills();
+
+    expect(wrapper.vm.$data.availableSkills).toHaveLength(1);
   });
-  it("correctly opens and closes the modal for a selected candidate", async () => {
-    const wrapper = mount(Candidates, {
-      global: {
-        mocks: {
-          $route: mockRoute,
-          $router: mockRouter,
-        },
-      },
-      stubs: ["router-link", "router-view"],
+
+  test("openModal", () => {
+    const wrapper = mount(Candidates);
+
+    wrapper.vm.openModal({
+      Staff_ID: 1,
+      Staff_FName: "John",
+      Staff_LName: "Doe",
     });
-    await wrapper.vm.$nextTick();
 
-    // Simulate selecting a candidate and opening the modal
-    wrapper.vm.openModal(/* Selected candidate data */);
+    expect(wrapper.vm.$data.selectedCandidate).toBeTruthy();
+  });
 
-    // Assert that the modal is open and displays the correct data
+  test("closeModal", () => {
+    const wrapper = mount(Candidates);
 
-    // Simulate closing the modal
+    wrapper.vm.openModal({
+      Staff_ID: 1,
+      Staff_FName: "John",
+      Staff_LName: "Doe",
+    });
+
     wrapper.vm.closeModal();
 
-    // Assert that the modal is closed
-    expect(wrapper.vm.selectedCandidate).toBeNull();
+    expect(wrapper.vm.$data.selectedCandidate).toBeNull();
   });
 
-  it('should call clearUserSessionData when the "Logout" button is clicked', () => {
-   const wrapper = mount(Candidates, {
-      global: {
-        mocks: {
-          $route: mockRoute,
-          $router: mockRouter,
-        },
-      },
-      stubs: ["router-link", "router-view"],
-    });
-    const clearUserSessionData = vi.spyOn(wrapper.vm, 'clearUserSessionData');
-    
-    const logoutButton = wrapper.find('button.btn-secondary');
-    logoutButton.trigger('click');
+  test("sortedCandidates", () => {
+    const wrapper = mount(Candidates);
 
-    expect(clearUserSessionData).toHaveBeenCalled();
+    wrapper.setData({
+      applications: [
+        { Application_Date: new Date(2022, 1, 1), Listing_ID: 1 },
+        { Application_Date: new Date(2021, 1, 1), Listing_ID: 1 },
+      ],
+      selectedRole: {
+        Listing_ID: 1,
+        Role_Name: "Software Engineer",
+      },
+    });
+
+    expect(wrapper.vm.sortedCandidates[0].Application_Date).toBe(
+      new Date(2021, 1, 1)
+    );
   });
 
-  it('should correctly calculate the percentage of skills matched', () => {
-    const wrapper = mount(Candidates, {
-      global: {
-        mocks: {
-          $route: mockRoute,
-          $router: mockRouter,
-        },
-      },
-      stubs: ["router-link", "router-view"],
-    });
-    const candidate = {
-      roleListing: {
-        Skills: ['Skill1', 'Skill2', 'Skill3']
-      },
-      Skills: [
-        { Skill_Name: 'Skill1' },
-        { Skill_Name: 'Skill3' }
-      ]
-    };
+  test("filteredCandidates", () => {
+    const wrapper = mount(Candidates);
 
-    const percentage = wrapper.vm.getPercentageSkillsMatched(candidate);
-
-    // In this example, 2 out of 3 skills match, so the percentage should be (2/3) * 100 = 66.67
-    expect(percentage).toBeCloseTo(66.67, 2); // Ensure the value is close to 66.67 with 2 decimal places precision
-  });
-
-  it('should filter candidates based on selected skills', () => {
-    const wrapper = mount(Candidates, {
-      global: {
-        mocks: {
-          $route: mockRoute,
-          $router: mockRouter,
-        },
-      },
-      stubs: ["router-link", "router-view"],
+    wrapper.setData({
+      rolecandidates: [
+        { Skills: [{ Skill_Name: "Vue.js" }, { Skill_Name: "JavaScript" }] },
+        { Skills: [{ Skill_Name: "Vue.js" }, { Skill_Name: "Python" }] },
+      ],
+      selectedSkills: ["Vue.js", "JavaScript"],
     });
 
-    // Assuming you have candidates with specific skills in your data
-    const candidate1 = { Skills: [{ Skill_Name: 'Skill1' }, { Skill_Name: 'Skill2' }] };
-    const candidate2 = { Skills: [{ Skill_Name: 'Skill2' }, { Skill_Name: 'Skill3' }] };
-    const candidate3 = { Skills: [{ Skill_Name: 'Skill1' }, { Skill_Name: 'Skill3' }] };
-
-    wrapper.setData({ rolecandidates: [candidate1, candidate2, candidate3] });
-
-    // Select a specific skill
-    wrapper.setData({ selectedSkills: ['Skill2'] });
-
-    // After filtering, only candidate1 and candidate2 should be visible
-    expect(wrapper.vm.filteredCandidates).toEqual([candidate1, candidate2]);
+    expect(wrapper.vm.filteredCandidates).toHaveLength(1);
   });
 });
