c161df8e0e52fcdcab7903783844d10a4d396d57
kaeeezia <91945302+kaeeezia@users.noreply.github.com>
2023-11-09 01:14:13 +0800

    integration testing

--- frontend/vitest.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest'
-
-export default defineConfig({
-    test: {
-        files: {
-            include: ['**/test/**/*', 'src/**/*.*.test.js'],
-        }
-    }
-})
